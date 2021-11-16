6f84daf47ff6293ae3d74944c1ab7c9e20a78cc1
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-11-16 14:39:14 +0100

    init db

---
+++ reset-db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `notes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `content` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
