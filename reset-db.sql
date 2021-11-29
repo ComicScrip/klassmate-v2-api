b0914083069b76ca0cb275e97c4ae4163dc57810
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-11-29 17:04:49 +0100

    persist data on users and notes

--- reset-db.sql
+++ reset-db.sql
@@ -1,6 +1,23 @@
+DROP TABLE IF EXISTS `notes`;
 CREATE TABLE `notes` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `content` TEXT NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstName` VARCHAR(255) NOT NULL,
+  `lastName` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(255) NOT NULL DEFAULT 'student',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+INSERT INTO
+  `users` (firstName, lastName)
+VALUES
+  ('John', 'Doe'),
+  ('Jane', 'Doe'),
+  ('Dave', 'Lopper'),
+  ('Luke', 'Skywalker'),
+  ('Ken', 'Adams')
